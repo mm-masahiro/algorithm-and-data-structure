d0f5e8adea936286393e788530b71b785edcbef4
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-08-11 23:02:09 +0900

    serach min value

--- chap3/linerSearch.js
+++ chap3/linerSearch.js
@@ -1,9 +1,11 @@
 var linerSearch = function (nums, target) {
+    var min_value = nums[0];
     for (var i = 0; i < nums.length - 1; i++) {
-        if (nums[i] === target)
-            return true;
+        // if (nums[i] === target) return true;
+        if (nums[i] < min_value)
+            min_value = nums[i];
     }
-    return false;
+    return min_value;
 };
 var nums = [2, 4, 1, 6];
 console.log(linerSearch(nums, 3));
--- chap3/linerSearch.ts
+++ chap3/linerSearch.ts
@@ -1,8 +1,10 @@
-const linerSearch = (nums: number[], target: number): boolean => {
+const linerSearch = (nums: number[], target: number): number => {
+  let min_value = nums[0];
   for(let i = 0; i < nums.length -1; i++) {
-    if (nums[i] === target) return true;
+    // if (nums[i] === target) return true;
+    if (nums[i] < min_value) min_value = nums[i]
   }
-  return false;
+  return min_value;
 };
 
 const nums = [2, 4, 1, 6]
